65a8424c34076393ed10d6f37d99fb16b2be1f25
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-08-02 04:28:27 -0700

    Pages Done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './components/HomePage';
+import Categories from './components/Categories';
 
-function App() {
-}
+const App = () => (
+  <Router>
+    <Routes>
+      {/* Each Route component defines a specific route and the component to render */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/category" element={<Categories />} />
+    </Routes>
+  </Router>
+);
 
 export default App;

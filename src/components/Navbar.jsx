7fc33c2f92e05be9da44519a6100a844db6b67ac
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-08-04 16:28:44 -0700

    Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 
 const Navbar = () => (
   <>
     <nav>
-      <h1>Bookstore CMS</h1>
-      <div>
+      <div className="nav">
+        <h1>Bookstore CMS</h1>
         <ul>
-          <li><NavLink to="/" className="nav-link">Home</NavLink></li>
+          <li><NavLink to="/" className="nav-link">Books</NavLink></li>
           <li><NavLink to="/categories" className="nav-link">Categories</NavLink></li>
         </ul>
       </div>
+      <FaUser />
     </nav>
   </>
 );
